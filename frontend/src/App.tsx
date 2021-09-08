d0ab7aa97b5fb7b9ef3eb2fe63cd802ac45ed40f
Rodrigo Victor <rodrigovictor81@gmail.com>
2021-09-08 17:29:38 -0300

    basic components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { DataTable } from "components/DataTable";
+import { Footer } from "components/Footer";
+import { Navbar } from "components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-primary">Letícia né?</h1>
+      <Navbar />
+      <h1>Oie</h1>
+      <DataTable />
+      <Footer />
     </>
   );
 }
