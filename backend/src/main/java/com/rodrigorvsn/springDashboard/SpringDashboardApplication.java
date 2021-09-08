67783913e087d9ed592611610b386f9cb052ee12
Rodrigo Victor <rodrigovictor81@gmail.com>
2021-09-08 16:53:42 -0300

    initial project template

---
+++ backend/src/main/java/com/rodrigorvsn/springDashboard/SpringDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigorvsn.springDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rodrigorvsn/springDashboard/SpringDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigorvsn.springDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
