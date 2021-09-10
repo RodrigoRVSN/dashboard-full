389cc35c6cbbcb4e65d4e9cb6c55985a6d68c0a2
Rodrigo Victor <rodrigovictor81@gmail.com>
2021-09-10 17:03:11 -0300

    BarChart Integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,56 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { BASE_URL } from "utils/requests";
+import { roundNumber } from "utils/roundNumber";
+
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 
 export function BarChart() {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((res) => {
+      const data = res.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) =>
+        roundNumber((100 * x.deals) / x.visited, 1)
+      );
+      setChartData({
+        labels: {
+          categories: myLabels,
+        },
+        series: [
+          {
+            name: "% sucesso",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
+
   const options = {
     plotOptions: {
       bar: {
@@ -9,22 +59,11 @@ export function BarChart() {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1],
-      },
-    ],
-  };
   return (
     <>
       <Chart
-        options={{ ...options, xaxis: mockData.labels }}
-        series={mockData.series}
+        options={{ ...options, xaxis: chartData.labels }}
+        series={chartData.series}
         type="bar"
         height="240"
       />
---
+++ frontend/src/utils/roundNumber.ts
@@ -0,0 +1,4 @@
+export const roundNumber = (value: number, precision: number) => {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+};
