85a28acb288dfc482f74f28b51c87d24ff824926
Rodrigo Victor <rodrigovictor81@gmail.com>
2021-09-10 08:12:02 -0300

    Group by search

--- backend/src/main/java/com/rodrigorvsn/springDashboard/repositories/SaleRepository.java
+++ backend/src/main/java/com/rodrigorvsn/springDashboard/repositories/SaleRepository.java
@@ -1,9 +1,22 @@
 package com.rodrigorvsn.springDashboard.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.rodrigorvsn.springDashboard.dto.SaleSuccessDTO;
+import com.rodrigorvsn.springDashboard.dto.SaleSumDTO;
 import com.rodrigorvsn.springDashboard.entities.Sale;
 
 public interface SaleRepository  extends JpaRepository <Sale, Long>{
+	
+	@Query("SELECT new com.rodrigorvsn.springDashboard.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
 
+	@Query("SELECT new com.rodrigorvsn.springDashboard.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/rodrigorvsn/springDashboard/service/SaleService.java
+++ backend/src/main/java/com/rodrigorvsn/springDashboard/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.rodrigorvsn.springDashboard.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rodrigorvsn.springDashboard.dto.SaleDTO;
+import com.rodrigorvsn.springDashboard.dto.SaleSuccessDTO;
+import com.rodrigorvsn.springDashboard.dto.SaleSumDTO;
 import com.rodrigorvsn.springDashboard.entities.Sale;
 import com.rodrigorvsn.springDashboard.repositories.SaleRepository;
 import com.rodrigorvsn.springDashboard.repositories.SellerRepository;
@@ -20,10 +24,20 @@ public class SaleService {
 	@Autowired
 	private SellerRepository sellerRepository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
